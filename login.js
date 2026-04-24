aa37e869f4d0a3ff1c324819a89a2a1aa36b16f0
ApenasUmBrasileiro <00001122929468sp@al.educacao.sp.gov.br>
2026-04-24 16:19:50 -0300

    versao final do projeto

---
+++ login.js
@@ -0,0 +1,38 @@
+const inputUsuario = document.getElementById('username')
+const inputSenha = document.getElementById('password')
+const botaoLogar = document.getElementById('logar')
+
+const usuarioADM = 'admin'
+const senhaADM = "admin"
+
+botaoLogar.addEventListener('click', () => {
+
+    let campoUsuario = inputUsuario.value
+    let campoSenha = inputSenha.value
+
+    if(campoUsuario.trim() == "" || campoUsuario.trim() != usuarioADM){
+    
+        //alert('O CAMPO DO USUÁRIO ESTÁ VAZIO')
+        let estiloAntigo = inputUsuario.style
+
+        inputUsuario.style = "border: red 1px solid; transition: all 0.5s ease"
+
+
+        setTimeout(() => {
+            inputUsuario.style = estiloAntigo 
+        }, 3000)
+
+        if(campoSenha.trim() == "" || campoSenha.trim() != senhaADM) {
+            let estiloAntigo = inputSenha.style
+            inputSenha.style = "border: red 1px solid; transition: all 0.5s ease"
+
+        setTimeout(() => {
+            inputSenha.style = estiloAntigo 
+        }, 3000)
+      }
+    }
+    if(campoUsuario == usuarioADM && campoSenha == senhaADM){
+        window.location.href = '/user.html'
+    }
+   }
+  )
